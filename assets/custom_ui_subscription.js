38ade758713decf8e193e40cefe0b16e58439990
Kostas McDade <mcdade.kostas@gmail.com>
2016-02-24 11:46:05 -0800

    Slider complete. Added in labels. Need responsive fix since it breaks.

---
+++ assets/custom_ui_subscription.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+	$( "#sliderpreferences_white_red" ).slider();
+	$( "#sliderpreferences_dry_sweet" ).slider();
+	$( "#sliderpreferences_complex_simple" ).slider();
+});
+
